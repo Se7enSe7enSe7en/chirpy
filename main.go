398ac557fcafea7cfb0759810b68cc45c4445075
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2024-10-30 06:50:03 +0800

    feat: added handleReadiness handler
    refactor: moved the fileserver api root to "/app/"

--- main.go
+++ main.go
@@ -5,22 +5,22 @@ import (
 	"net/http"
 )
 
-type apiHandler struct {
-}
-
-func (a apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
-	log.Fatal("unimplemented")
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func main() {
 	const filepathRoot = "."
-	const logoPath = "assets/logo.png"
 	const port = "8080"
 
 	// create server mux
 	mux := http.NewServeMux()
-	// handle root path "/" then use http.FileServer to call the index.html in the "." directory
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	// connect handlers
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	// init the server config
 	s := &http.Server{

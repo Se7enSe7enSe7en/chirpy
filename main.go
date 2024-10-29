296c6bad9f04939fd85d8f461cdc0a2a99f50ce0
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2024-10-29 09:11:54 +0800

    feat: serve index.html

--- main.go
+++ main.go
@@ -5,19 +5,29 @@ import (
 	"net/http"
 )
 
+type apiHandler struct {
+}
+
+func (a apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+	log.Fatal("unimplemented")
+}
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	// create server mux
 	mux := http.NewServeMux()
+	// handle root path "/" then use http.FileServer to call the index.html in the "." directory
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
-	// init the server
+	// init the server config
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	// start the server
-	log.Printf("Serving in port: %v\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatalln(s.ListenAndServe())
 }

e7164a0bc8f29b36cc5bf6717019c0bca13af010
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2024-11-15 22:43:05 +0800

    refactor: clean up author id query param

--- handler_chirp_list.go
+++ handler_chirp_list.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpList(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	authorId := uuid.Nil
+	authorIdStr := r.URL.Query().Get("author_id")
+	if authorIdStr != "" {
+		authorId, err = uuid.Parse(authorIdStr)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Cannot parse author_id query param", err)
+		}
+	}
+
 	chirpList, err := cfg.db.GetChirpList(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirp list", err)
@@ -13,6 +26,13 @@ func (cfg *apiConfig) handlerChirpList(w http.ResponseWriter, r *http.Request) {
 
 	responseList := make([]Chirp, len(chirpList))
 	for i, chirp := range chirpList {
+		// optional filter: authorId
+		if authorId != uuid.Nil && authorId != chirp.UserID {
+			// if authorId exists and authorId is not equal to the chirp.UserID
+			// then skip
+			continue
+		}
+
 		responseList[i] = Chirp{
 			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,

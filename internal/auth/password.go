5daf0716b66db6560fcc3782d307163de99541cc
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2024-11-24 13:07:00 +0800

    refactor: used bcrypt default cost

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -3,7 +3,7 @@ package auth
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), 0)
+	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
